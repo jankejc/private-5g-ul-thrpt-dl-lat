020d4298bbadf7f402c61b94cb523d3ad1ffa678
jankejc <jkornacki@o2.pl>
2025-03-17 23:42:01 +0100

    name the object 3/3

--- script/hosts/service_recv_vxlan_linux_host.py
+++ script/hosts/service_recv_vxlan_linux_host.py
@@ -1,16 +1,15 @@
 import time
-from typing import Optional
 
-from hosts.ntp_ip_node import NtpIpNode
 from utils import print_info, print_success, print_error
 from hosts.vxlan_linux_host import VxlanHost, VxlanLinuxHost
 
 
 class ServiceRecvVxlanLinuxHost(VxlanLinuxHost):
     """I want only service receiver (Lenovo) to run tests."""
-    def __init__(self, vxlan_if: str, **kwargs):
+    def __init__(self, vxlan_if: str, receiver_name: str, **kwargs):
         super().__init__(**kwargs)
         self.vxlan_if = vxlan_if
+        self.receiver_name = receiver_name
 
     def run_vxlan_ping_test(self,
                             dest_host: VxlanHost,
@@ -31,7 +30,7 @@ class ServiceRecvVxlanLinuxHost(VxlanLinuxHost):
             pcap_file = f"{dynamic_log_dir}/{ping_size}/pcap/{filename}.pcap" if save_pcap else None
 
             print_info(
-                f"Running Ping on {self.vxlan_ip} / {self.management_ip} "
+                f"[{self.receiver_name}] Running Ping on {self.vxlan_ip} / {self.management_ip} "
                 f"to {dest_host.vxlan_ip} / {dest_host.management_ip} with packet size {ping_size}, "
                 f"count {ping_count}, interval {self.min_ping_interval}s...")
 
@@ -56,14 +55,14 @@ class ServiceRecvVxlanLinuxHost(VxlanLinuxHost):
                 self.execute_command(f"sudo pkill -f 'tcpdump -ni {self.vxlan_if}'")
 
             if exit_status == 0:
-                msg = f"Ping Packet Size {ping_size} completed successfully."
+                msg = f"[{self.receiver_name}] Ping Packet Size {ping_size} completed successfully."
                 if save_pcap:
                     msg += f" Pcap saved as {pcap_file}."
                 print_success(msg)
                 return True
             else:
-                print_error(f"Ping for Packet Size {ping_size} failed: {stderr}")
+                print_error(f"[{self.receiver_name}] Ping for Packet Size {ping_size} failed: {stderr}")
                 return False
         except Exception as e:
-            print_error(f"Exception during Ping for Packet Size {ping_size}: {e}")
+            print_error(f"[{self.receiver_name}] Exception during Ping for Packet Size {ping_size}: {e}")
             return False
